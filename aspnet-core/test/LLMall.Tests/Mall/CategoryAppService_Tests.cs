77003e97fb5d93a67bc187aaf0de87d3d66f88c8
sunven <604164227@qq.com>
2019-06-27 22:38:47 +0800

    add unit test

---
+++ aspnet-core/test/LLMall.Tests/Mall/CategoryAppService_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+using Abp.Application.Services.Dto;
+using LLMall.Users;
+using LLMall.Users.Dto;
+using LLMall.Mall;
+using LLMall.Mall.Dto;
+
+namespace LLMall.Tests.Users
+{
+    public class BannerAppService_Tests : LLMallTestBase
+    {
+        private readonly IBannerAppService _bannerAppService;
+
+        public BannerAppService_Tests()
+        {
+            _bannerAppService = Resolve<IBannerAppService>();
+        }
+
+        [Fact]
+        public async Task Add_Test()
+        {
+            // Act
+            var output = await _bannerAppService.Add(new BannerDto { Name = "1", PicUrl = "2" });
+
+            // Assert
+            Assert.NotEqual(0, output.Id);
+
+            //output.Items.Count.ShouldBeGreaterThan(0);
+        }
+    }
+}
