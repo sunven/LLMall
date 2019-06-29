e0564e762103fef23c4dbb8a253fc9e63b31b89d
sunwq <604164227@qq.com>
2019-06-29 18:13:12 +0800

    add psql

---
+++ aspnet-core/src/LLMall.Application/Mall/AchievementAppService.cs
@@ -0,0 +1,7 @@
+namespace LLMall.Mall
+{
+    public class AchievementAppService
+    {
+        
+    }
+}
---
+++ aspnet-core/src/LLMall.Application/Mall/IAchievementAppService.cs
@@ -0,0 +1,7 @@
+namespace LLMall.Mall
+{
+    public interface IAchievementAppService
+    {
+        
+    }
+}
